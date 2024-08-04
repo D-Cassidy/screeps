53e020d5f0392ed49fa43c248b0f6e64a9575dd9
D-Cassidy <declanpcassidy1@gmail.com>
2024-08-04 16:09:48 -0400

    add basic auto construction of drones

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
 var roleHarvester = require('role.harvester');
 
+// TODO:
+// - Automatic drone construction
+// - Role deliberation
+// - Harvester role 
+// - Upgrader role
+
 module.exports.loop = function () {
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
+    for (let name in Game.spawns) {
+        let spawn = Game.spawns[name];
+
+        if (spawn.store.getUsedCapacity(RESOURCE_ENERGY) < 250) {
+            let roleName = 'harvester';
+            let creepName = getCreepName(roleName);
+            spawn.spawnCreep([WORK, CARRY, MOVE, MOVE], creepName, {
+                memory: {role: roleName}
+            });
+        }
+    }
+
+    for (let name in Game.creeps) {
+        let creep = Game.creeps[name];
         roleHarvester.run(creep);
     }
+}
+
+function getCreepName(role) {
+    return (`Drone-${role[0].toUpperCase()}${Game.time%1000}`);
 }
