6fa1d04539f04412d13941ee5dc83e0107f55187
D-Cassidy <declanpcassidy1@gmail.com>
2024-08-05 21:57:29 -0400

    add usage of creep.memory.working to role method

--- role.harvester.js
+++ role.harvester.js
@@ -1,16 +1,23 @@
 let roleHarvester = {
 
     run: function(creep) {
-        if(creep.store.getFreeCapacity() > 0) {
-            let sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+
+        // If working == false, fill up with energy
+        if (!working) {
+            let source = creep.pos.findClosestByPath(FIND_SOURCES);
+            if (creep.harvest(source[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source[0]);
             }
         }
+
+        // If working == true, get to work
         else {
-            let spawnName = creep.memory.home;
-            let spawn = Game.spawns[spawnName]
-            if(creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            if (creep.getUsedCapacity(RESOURCE_ENERGY) <= 0) {
+                creep.memory.working = false;
+            }
+
+            let spawn = Game.spawns[creep.memory.home];
+            if (creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(spawn);
             }
         }
