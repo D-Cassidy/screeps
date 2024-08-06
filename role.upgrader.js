53ee798785feb333116ed59e9717fe0d57ef4c2b
D-Cassidy <declanpcassidy1@gmail.com>
2024-08-05 22:14:51 -0400

    add basic role.upgrader script

---
+++ role.upgrader.js
@@ -0,0 +1,33 @@
+let roleUpgrader = {
+
+    run: function(creep) {
+        let working = creep.memory.working;
+
+        if (!working) {
+            if (creep.store.getFreeCapacity(RESOURCE_ENERGY) <= 0) {
+                creep.memory.working = true;
+            }
+            else { harvest(creep); }
+        }
+
+        else {
+            if (creep.store.getUsedCapacity(RESOURCE_ENERGY) <= 0) {
+                creep.memory.working = false;
+            }
+
+            let controller = creep.room.controller;
+            if (creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(controller);
+            }
+        }
+    }
+};
+
+function harvest(creep) {
+    let source = creep.pos.findClosestByPath(FIND_SOURCES);
+    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source);
+    }
+};
+
+module.exports = roleUpgrader;
