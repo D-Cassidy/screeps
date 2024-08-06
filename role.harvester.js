7b838110a9b39d7a398c2a7ebc986969a1bf7763
D-Cassidy <declanpcassidy1@gmail.com>
2024-08-05 22:09:13 -0400

    fix bad refactor

--- role.harvester.js
+++ role.harvester.js
@@ -23,16 +23,15 @@ let roleHarvester = {
 };
 
 function harvest(creep) {
-    if (!working) {
-        if (creep.store.getFreeCapacity(RESOURCE_ENERGY) <= 0) {
-            creep.memory.working = true;
-        }
-
-        let source = creep.pos.findClosestByPath(FIND_SOURCES);
-        if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(source);
-        }
+    if (creep.store.getFreeCapacity(RESOURCE_ENERGY) <= 0) {
+        creep.memory.working = true;
     }
+
+    let source = creep.pos.findClosestByPath(FIND_SOURCES);
+    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source);
+    }
+    
 };
 
 module.exports = roleHarvester;
