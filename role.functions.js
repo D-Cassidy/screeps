bd231785c0eae238f27d778c57a894dd7970084e
D-Cassidy <declanpcassidy1@gmail.com>
2024-08-07 10:41:36 -0400

    add prioritization of containers with more energy

--- role.functions.js
+++ role.functions.js
@@ -7,7 +7,10 @@ let roleFunctions = {
                 && s.store.getUsedCapacity(RESOURCE_ENERGY) > 0 
         );
         if (containers.length > 0) {
-            let target = creep.pos.findClosestByPath(containers);
+            // this line sorts the containers by their capacity, prioritizing use of fuller containers
+            let target = creep.room.find(FIND_STRUCTURES).filter((s) => 
+                s.structureType == STRUCTURE_CONTAINER).sort((s1, s2) => 
+                    s2.store.getUsedCapacity(RESOURCE_ENERGY) - s1.store.getUsedCapacity(RESOURCE_ENERGY))[0];
             if (creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(target);
             }
