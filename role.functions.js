f732160de3e8d2d0bbe387a593b31690848fe22d
D-Cassidy <declanpcassidy1@gmail.com>
2024-08-07 10:42:08 -0400

    remove leftover print statement from debugging

--- role.functions.js
+++ role.functions.js
@@ -14,7 +14,6 @@ let roleFunctions = {
             if (creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(target);
             }
-            console.log('here?')
             return;
         }
 
