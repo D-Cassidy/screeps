b3e4f307435722ec64cd6179938ea4177f906934
D-Cassidy <declanpcassidy1@gmail.com>
2024-08-07 10:05:50 -0400

    fix issue with withdrawing from containers

--- role.functions.js
+++ role.functions.js
@@ -2,15 +2,16 @@ let roleFunctions = {
 
     harvest: function(creep) {
         // check for containers with energy
-        let containers = creep.room.find(FIND_MY_STRUCTURES).filter((s) => 
+        let containers = creep.room.find(FIND_STRUCTURES).filter((s) => 
                 s.structureType == STRUCTURE_CONTAINER
                 && s.store.getUsedCapacity(RESOURCE_ENERGY) > 0 
         );
         if (containers.length > 0) {
             let target = creep.pos.findClosestByPath(containers);
-            if (creep.withdraw(target) == ERR_NOT_IN_RANGE) {
+            if (creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(target);
             }
+            console.log('here?')
             return;
         }
 
