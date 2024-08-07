5323d118518122590efede7564889359d27700a7
D-Cassidy <declanpcassidy1@gmail.com>
2024-08-06 21:56:19 -0400

    fix bug with repairs not working on roads

--- struct.tower.js
+++ struct.tower.js
@@ -3,15 +3,15 @@ let structTower = {
     run: function(tower) {
         // attack closest hostile if there's one in the room
         let hostiles = tower.room.find(FIND_HOSTILE_CREEPS);
-        if (hostiles) {
+        if (hostiles.length > 0) {
             tower.attack(tower.pos.findClosestByRange(hostiles));
             return;
         }
 
         // then, repair structures
-        let repairableStructs = tower.room.find(FIND_MY_STRUCTURES).filter((s) => s.hits < s.hitsMax);
-        if (repairableStructs) {
-            tower.repair(repaireableStructs[0]);
+        let repairableStructs = tower.room.find(FIND_STRUCTURES).filter((s) => s.hits < s.hitsMax);
+        if (repairableStructs.length > 0) {
+            tower.repair(repairableStructs[0]);
             return;
         }
     }
